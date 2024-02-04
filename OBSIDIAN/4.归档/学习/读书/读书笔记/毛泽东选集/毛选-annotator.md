8271f89bb6f68333db6271cdb1cd04ccce23efb4
coderyjc <coderyjc@163.com>
2024-02-04 23:46:45 +0800

    vault backup: 2024-02-04 23:46:45

--- OBSIDIAN/4.归档/学习/读书/读书笔记/毛泽东选集/毛选-annotator.md
+++ OBSIDIAN/4.归档/学习/读书/读书笔记/毛泽东选集/毛选-annotator.md
@@ -1,5 +1,5 @@
 ---
-annotation-target: 4.归档\学习\读书\读书笔记\毛泽东选集\毛选.pdf
+annotation-target: 毛选.pdf
 ---
 
 
--- scripts/Statistic.py
+++ scripts/Statistic.py
@@ -3,13 +3,38 @@ import markdown2
 import re
 from zhon import hanzi
 
-def recursive_dict_print(d, indent=0):
+def output_details(d):
     sorted_data = sorted(d.values(), key=lambda x: x['size'], reverse=True)
     print("| 文件类型 | 数量 | 大小 |")
     print("|----------|-------|------|")
     for item in sorted_data:
-        print("| {} | {} | {:.2f} MB |".format(item['name'], item['count'], item['size']))
+        print("| {} | {} | {:.2f} MBytes |".format(item['name'], item['count'], item['size']))
 
+def output_overall(d):
+    type_dict = {
+        '笔记': {'name': '笔记', 'count': 0, 'size': 0},
+        '书籍': {'name': '书籍', 'count': 0, 'size': 0},
+        '图片': {'name': '图片', 'count': 0, 'size': 0},
+        '插件相关': {'name': '插件相关', 'count': 0, 'size': 0},
+        '其他': {'name': '其他', 'count': 0, 'size': 0}
+    }
+
+    file_types = {
+        'md': '笔记', 'pdf': '书籍', 'png': '图片', 'jpg': '图片', 
+        'jpeg': '图片', 'gif': '图片', 'drawio': '图片', 'webp': '图片', 'xmind': '图片',
+        'js': '插件相关', 'css': '插件相关', 'scss': '插件相关', 'ts': '插件相关'
+    }
+
+    for key, value in d.items():
+        file_type = file_types.get(key, '其他')
+        type_dict[file_type]['count'] += value['count']
+        type_dict[file_type]['size'] += value['size']
+
+    sorted_data = sorted(type_dict.values(), key=lambda x: x['size'], reverse=True)
+    print("| 文件类型 | 数量 | 大小 |")
+    print("|----------|-------|------|")
+    for item in sorted_data:
+        print("| {} | {} | {:.2f} MBytes |".format(item['name'], item['count'], item['size']))
 
 
 def count_words_md(file_path):
@@ -18,12 +43,10 @@ def count_words_md(file_path):
 
         # 统计中文字符数量
         chinese_characters = re.findall(f'[{hanzi.characters}]', content)
-
         # 统计英文单词数量
         english_words = re.findall(r'\b[A-Za-z]+\b', content)
 
         total_words = len(chinese_characters) + len(english_words)
-
         return total_words
 
 def process_folder(folder_path):
@@ -58,12 +81,7 @@ note_folder_path = '../OBSIDIAN'
 
 file_types_count, total_md_words = process_folder(note_folder_path)
 
-
-recursive_dict_print(file_types_count)
-
-# print("文件类型统计:")
-# for file_type, count in file_types_count.items():
-#     print(f"{file_type}: {count} 个文件")
+output_overall(file_types_count)
 
 print("\n所有MD文件字数总和:")
 print(f"{total_md_words} 个字")
